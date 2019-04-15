e30f8cad20c1465937117271de0f02437ebdd7e8
GuilhermeCaruso <guilherme.caruso@owldocs.com>
2019-04-15 20:49:35 -0300

    tools/rotation: add rotation methods

--- main.go
+++ main.go
@@ -20,28 +20,8 @@ func main() {
 		log.Fatal(os.Stderr, "%s: %v\n", "flower.jpg", err)
 	}
 
-	// bounds := img.Bounds()
-	// newIMG := image.NewRGBA(bounds)
-
-	// fmt.Println(math.Cos((90*135.089)/180)*(float64(3-1.5)) - math.Sin((90*135.089)/180)*(float64(3-1.5)) + float64(1.5))
-	// // Loop in all reference and new image pixels
-	// for y := 0; y < bounds.Max.Y; y++ {
-	// 	for x := 0; x < bounds.Max.X; x++ {
-	// 		actualPixel := img.At(x, y)
-	// 		var anglen float64 = 270
-
-	// 		angle := (anglen * 135.089) / 180
-	// 		newIMG.Set(int(math.Cos(angle)*(float64(x-bounds.Max.X/2))-math.Sin(angle)*(float64(y-bounds.Max.Y/2))+float64(bounds.Max.X/2)),
-	// 			int(math.Sin(angle)*(float64(x-bounds.Max.X/2))+math.Cos(angle)*(float64(y-bounds.Max.Y/2))+float64(bounds.Max.Y/2)), actualPixel)
-
-	// 	}
-	// }
-
-	newIMG := tools.Rotate(img, 90)
-
-	// fmt.Println((float64(300) * math.Cos(0.1)) - (float64(300) * math.Sin(0.1)))
-	// fmt.Println((float64(300) * math.Sin(0.1)) + (float64(300) * math.Cos(0.1)))
-	// flippedImg := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{10.10, 1600}})
+	newIMG := tools.Rotate(img, 0)
+	newIMG = tools.Threshold(newIMG, 100)
 
 	outFile, err := os.Create("changed.jpg")
 	if err != nil {
